4fb7619bd63f8f68df03c22d00d5dda04ec22c0b
IanPedroV <ian0198@gmail.com>
2018-05-24 00:26:24 -0300

    Refactor: changed design a little and created feed page

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,8 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
-
 import { MyApp } from './app.component';
- import { HomePage } from '../pages/home/home';
+import { HomePage } from '../pages/home/home';
 import { TabsPage } from '../pages/tabs/tabs';
 import {ProductsPage} from "../pages/products/products";
 
--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -1,16 +1,16 @@
-import { Component } from '@angular/core';
-import { NavParams } from 'ionic-angular';
-import { HomePage } from '../home/home';
+import {Component} from '@angular/core';
+import {NavParams} from 'ionic-angular';
+import {HomePage} from '../home/home';
 import {ProductsPage} from "../products/products";
 
 @Component({
   selector: 'page-tabs',
   templateUrl: 'tabs.html',
 })
-  export class TabsPage {
-  tab1Root: any = ProductsPage;
+export class TabsPage {
+  tab1Root: any = HomePage;
   tab2Root: any = HomePage;
-  tab3Root: any = HomePage;
+  tab3Root: any = ProductsPage;
   mySelectedIndex: number;
 
   constructor(navParams: NavParams) {
